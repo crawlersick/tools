dbb00860d18e02381bc60ee34b780e2bbb3d8e93
john <john.b.y.yang@gmail.com>
2015-12-11 01:47:00 +0800

    add list ana for kat

--- autodown/kat_fetch.py
+++ autodown/kat_fetch.py
@@ -11,4 +11,11 @@ else:
     print "start process {p1} for kat".format(p1=targetname)
 kats='https://kat.cr/usearch/'+targetname
 print "full url {p1} for kat".format(p1=kats)
+anares=anaurl.ana(kats,'data-sc-params="{ \'name\': \'(.*?)\', \'magnet\': \'(.*?)\' }"></div>')
+for i,e in enumerate(anares):
+    temp=list(anares[i])
+    temp[0]=urllib.unquote(temp[0])
+    temp[1]=urllib.unquote(temp[1])
+    anares[i]=tuple(temp)
 
+print anares
