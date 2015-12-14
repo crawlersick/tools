aac12c8e0a99192f80a61aae44a66e33f1a67980
john <root@sicktako.(none)>
2015-12-14 04:07:53 -0500

    ...

--- autodown/kat_fetch.sh
+++ autodown/kat_fetch.sh
@@ -8,7 +8,7 @@ if [ ! "$maglink" == 'None' ]
 then
 	link=`echo $maglink|awk -F"'"  '{print $4}'`
 	mkdir -p "$downloadfolder/$keyw"
-	aria2c -c -d "$downloadfolder/$keyw" --enable-dht=true --enable-dht6=true --enable-peer-exchange=true --follow-metalink=mem --seed-time=0 --max-overall-upload-limit=50K " "${link}" | tee "/tmp/$keyw.log"
+	aria2c -c -d "$downloadfolder/$keyw" --enable-dht=true --enable-dht6=true --enable-peer-exchange=true --follow-metalink=mem --seed-time=0 --max-overall-upload-limit=50K "${link}" | tee "/tmp/$keyw.log"
 else
 	echo 'not found'
 fi
