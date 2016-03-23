71956246ea665123fa05a65fd3a2d414fa1b9c18
john <john.b.y.yang@gmail.com>
2016-03-24 00:06:20 +0800

    add loop check

--- autodown/36dm.sh
+++ autodown/36dm.sh
@@ -175,7 +175,7 @@ then
 #	fi
 
 	mkdir -p "$downloadfolder/$keyw"
-        aria2c -c -d "$downloadfolder/$keyw" --log="/tmp/$keyw.log" --log-level=notice --enable-dht=true --enable-dht6=true --enable-peer-exchange=true --follow-metalink=mem --seed-time=0 --max-overall-upload-limit=50K --bt-tracker="http://open.nyaatorrents.info:6544/announce,http://tracker.haretahoo.science:2710/announce,udp://tracker.publicbt.com/announce,udp://glotorrents.pw:6969/announe,udp://tracker.openbittorrent.com:80/announce,udp://coppersurfer.tk:6969/announce,udp://p4p.arenabg.ch:1337" "$list11p" 
+        aria2c -c -d "$downloadfolder/$keyw" --log="/tmp/$keyw.log" --log-level=notice --enable-color=false --enable-dht=true --enable-dht6=true --enable-peer-exchange=true --follow-metalink=mem --seed-time=0 --max-overall-upload-limit=50K --bt-tracker="http://open.nyaatorrents.info:6544/announce,http://tracker.haretahoo.science:2710/announce,udp://tracker.publicbt.com/announce,udp://glotorrents.pw:6969/announe,udp://tracker.openbittorrent.com:80/announce,udp://coppersurfer.tk:6969/announce,udp://p4p.arenabg.ch:1337" "$list11p" 
 	#| tee "/tmp/$keyw.log"
 	recode=$?
 	if [[ $recode -eq 0 ]]
--- autodown/auto_down_bat.sh
+++ autodown/auto_down_bat.sh
@@ -9,7 +9,7 @@ fi
 IFS=$'\r\n'
 namelist=($(cat auto_ani.list))
 
-time_s=`expr "3600" '*' "1"`
+time_s=`expr "900" '*' "1"`
 
 while [[ true ]]
 do
@@ -17,18 +17,44 @@ do
 i=0
 	while [[ $i -lt ${#namelist[@]} ]]
 	do
-            THREAD_COUNT=$(ps | grep "36dm.sh" | wc -l)
-            while [ $THREAD_COUNT -ge $thrcnt ]
-            do
-            ps -f
-                sleep 180
-                THREAD_COUNT=$(ps | grep "36dm.sh" | wc -l)
-            done
-		
-	    36dm.sh "${namelist[i]}" &
-            sleep 30
-
-	i=`expr $i + 1`
+#            THREAD_COUNT=$(ps | grep "36dm.sh" | wc -l)
+#            while [ $THREAD_COUNT -ge $thrcnt ]
+#            do
+#            ps -f
+#                sleep 180
+#                THREAD_COUNT=$(ps | grep "36dm.sh" | wc -l)
+#            done
+#		
+#	    36dm.sh "${namelist[i]}" &
+#            sleep 30
+		keyw="${namelist[i]}"
+		THREAD_COUNT=$(ps -ef | grep "aria2c" | grep "$keyw" | wc -l)
+		if [[ $THREAD_COUNT -gt 0 ]]
+		then
+			ariapid=`ps -ef |grep aria2 | grep "$keyw"|tail -1| awk '{print $2}'`
+			runtime=`ps -p $ariapid -o etimes=`
+			if [[ $runtime -gt 8000 ]]
+			then
+				echo "$keyw runtime $runtime , kill it now."
+				kill -SIGINT  $ariapid
+				sleep 30
+			else
+				echo "$keyw"" is proccessing... skip this now."
+				i=`expr $i + 1`
+				continue
+			fi
+		fi
+		THREAD_COUNT=$(ps | grep "aria2c" | wc -l)
+		if [[ $THREAD_COUNT -ge $thrcnt ]]
+		then
+			echo 'aria2c thr greater then '$thrcnt' , continue'
+			sleep 30
+			i=`expr $i + 1`
+			continue
+		fi
+		36dm.sh "${namelist[i]}" &
+		sleep 30
+		i=`expr $i + 1`
 	done
 sleep $time_s
 namelist=($(cat auto_ani.list))
