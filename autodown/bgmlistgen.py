8b332ee213021b6062e1d00eaf0c7c9ca8d7c842
john <john.b.y.yang@gmail.com>
2016-09-30 14:15:57 +0800

    ..

--- autodown/bgmlistgen.py
+++ autodown/bgmlistgen.py
@@ -4,9 +4,9 @@ import re
 if __name__=='__main__':
     print('aa') 
     resu=libs.anaurl2.ana("http://www.kansou.me/",'<td align="center">([0-9]{4}/[0-9]{2}/[0-9]{2})\(.*?\)</td>.*?<td align="center"><a href="(.*?)" target=')
-    #print(resu)
+    print(resu)
     for a in resu:
         print(a[1])
-        tempstr=re.search('http://([^\.]+)',a[1])
-        print(tempstr)
+        tempstr=re.search('://[Ww\.]*([^\.]+)',a[1])
+        print(tempstr.group(1))
 
