4079eb6bc9c99a6b986c488d85076dc948c0b8fc
Your Name <you@example.com>
2019-10-08 02:45:17 +0800

    mm

--- autodown/call_anlsweb.sh
+++ autodown/call_anlsweb.sh
@@ -12,16 +12,21 @@ do
 	name=`echo $line |perl -ne 'print $2 if /(.*?)\|\|\|\|--\^\^--\|\|\|\|(.*?)\|\|\|\|--\^\^--\|\|\|\|(.*)/s'`
 	group=`echo $line |perl -ne 'print $1 if /(.*?)\|\|\|\|--\^\^--\|\|\|\|(.*?)\|\|\|\|--\^\^--\|\|\|\|(.*)/s'`
 
-	echo 'mag'
-	echo $mag
+	#echo 'mag'
+	#echo $mag
+	echo '-0--------------------------------'
 	echo 'name'
 	echo $name
 	echo 'group'
 	echo $group
-    epnum=`echo ${name}|grep -ioP '(?<=[^0-9a-zA-Z])[0-9_\.\(\)]+(?=[\]話话】 Vv])'| tr '\n' ' '`
-	echo 'epnum:'
+	epnum=`echo ${name}|grep -ioP '(?<=[^0-9a-zA-Z])[0-9_\.\(\)]+(?=[\]話话】 Vv章])'| tr '\n' ' '`
+	if [[ -z "$epnum" ]]
+	then
+            echo 'try match epnum as EP'
+            epnum=`echo ${name}|grep -ioP '(?<=EP)[0-9_\.\(\)]+(?=[ \[])'| tr '\n' ' '`
+	fi
+    echo 'epnum:'
     echo $epnum
-
     if [[ ! -z "$epnum" ]]
     then
         echo "$name" | grep -iq "$search_str"
@@ -29,10 +34,14 @@ do
         if [[ $re_code -eq 0 ]]
         then
             echo "found $name  ---   $epnum"
-
+	    if [[ ! -d "$HOME/Downloads/$search_str" ]]
+	    then
+		    mkdir -p "$HOME/Downloads/$search_str"
+		    chmod 777 "$HOME/Downloads/$search_str"
+	    fi
             trackers=`cat trackers.txt`
-            aria2c -c -d $HOME/Downloads --log="/tmp/$search_str.log" --log-level=notice --enable-color=false --enable-dht=true --enable-dht6=true --enable-peer-exchange=true --follow-metalink=mem --seed-time=10 --max-overall-upload-limit=50K --bt-tracker=$trackers $mag
-            
+            aria2c -c -d "$HOME/Downloads/$search_str" --log="/tmp/$search_str.log" --log-level=notice --enable-color=false --enable-dht=true --enable-dht6=true --enable-peer-exchange=true --follow-metalink=mem --seed-time=10 --max-overall-upload-limit=50K --bt-tracker=$trackers $mag
+	    chmod -R 777 "$HOME/Downloads/$search_str/*"
             exit 0
         fi
     fi
